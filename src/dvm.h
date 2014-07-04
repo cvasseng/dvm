34890ff89d81a090998f7241e3356dda16bde609
Chris MV <cvasseng@gmail.com>
2014-07-04 01:40:28 -0500

    Compiler stuff

--- src/dvm.h
+++ src/dvm.h
@@ -1,6 +1,12 @@
 #ifndef h__dvm__
 #define h__dvm__
 
+struct ProgramSource {
+	short program[2048];
+	int programSize;
+};
+
 extern void dvm_run(const short *prog, unsigned int size);
+extern ProgramSource dvm_compile(const char* filename);
 
 #endif
